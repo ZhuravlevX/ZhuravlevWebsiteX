592b1eb3b42550082028c30b552ca1742c7fb5d8
ZhuravlevX <danzhuravlevx@gmail.com>
2025-03-03 16:19:06 +0300

    Changed names of image folders

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,39 +14,42 @@ const octokit = new Octokit();
 const GITHUB_USERNAME = "ZhuravlevX";
 
 const artImages = [
-  "/lovable-uploads/0d1283f2-cb21-46e1-85b6-81be3521f8c3.png", 
-  "/lovable-uploads/19929ee8-5efd-4c53-b9da-b353539a72af.png", 
-  "/lovable-uploads/28f40eac-9977-491f-a0ef-ad90340b52f0.png", 
-  "/lovable-uploads/3337fad9-26e0-4f24-96b6-cace42d94c6d.png", 
-  "/lovable-uploads/538e6b34-1f0e-42d2-a066-8649a3e72357.png", 
-  "/lovable-uploads/5d0e8bc6-db59-4233-a985-e9c0b04c0d2a.png", 
-  "/lovable-uploads/6345fdb0-ab9f-4470-bf16-38d24bac30de.png", 
-  "/lovable-uploads/7877656a-74a2-442f-be3c-2835881ab0fa.png", 
-  "/lovable-uploads/8548b7ea-f8e3-4233-9d46-68cbafa653f1.png", 
-  "/lovable-uploads/92b6ff3e-3cfa-4548-825d-112c414c83c8.png", 
-  "/lovable-uploads/9a26b27c-cbae-4af2-9139-f4cdd28a7bfb.png", 
-  "/lovable-uploads/a923fea8-b3ec-4381-a0bd-d18039b48e1d.png", 
-  "/lovable-uploads/b9198cd9-b141-45c3-8323-ea4fcaaeb573.png", 
-  "/lovable-uploads/bd622df0-b04c-427f-82d8-0c78820b58ff.png", 
-  "/lovable-uploads/d819c9a8-1805-4f0c-9bc9-d8aa11b31751.png", 
-  "/lovable-uploads/dba87e47-e3b0-4c03-b987-18b9f3e03cca.png",
-  "/lovable-uploads/b2eaaa75-df25-4419-8b64-48f87be986ba.png",
-  "/lovable-uploads/812906ad-bccc-4206-a48c-a0f558e41f64.png",
-  "/lovable-uploads/2d412165-ecdf-4976-a142-5b00f95251cb.png",
-  "/lovable-uploads/3759d3cd-9eea-4303-b2de-511c1bc94827.png",
-  "/lovable-uploads/7571356b-dbb4-425c-a319-c5cb058d48fa.png"
+  "/gallery/arts/0d1283f2-cb21-46e1-85b6-81be3521f8c3.png",
+  "/gallery/arts/19929ee8-5efd-4c53-b9da-b353539a72af.png",
+  "/gallery/arts/28f40eac-9977-491f-a0ef-ad90340b52f0.png",
+  "/gallery/arts/3337fad9-26e0-4f24-96b6-cace42d94c6d.png",
+  "/gallery/arts/538e6b34-1f0e-42d2-a066-8649a3e72357.png",
+  "/gallery/arts/5d0e8bc6-db59-4233-a985-e9c0b04c0d2a.png",
+  "/gallery/arts/6345fdb0-ab9f-4470-bf16-38d24bac30de.png",
+  "/gallery/arts/7877656a-74a2-442f-be3c-2835881ab0fa.png",
+  "/gallery/arts/8548b7ea-f8e3-4233-9d46-68cbafa653f1.png",
+  "/gallery/arts/92b6ff3e-3cfa-4548-825d-112c414c83c8.png",
+  "/gallery/arts/9a26b27c-cbae-4af2-9139-f4cdd28a7bfb.png",
+  "/gallery/arts/a923fea8-b3ec-4381-a0bd-d18039b48e1d.png",
+  "/gallery/arts/b9198cd9-b141-45c3-8323-ea4fcaaeb573.png",
+  "/gallery/arts/bd622df0-b04c-427f-82d8-0c78820b58ff.png",
+  "/gallery/arts/d819c9a8-1805-4f0c-9bc9-d8aa11b31751.png",
+  "/gallery/arts/dba87e47-e3b0-4c03-b987-18b9f3e03cca.png",
+  "/gallery/arts/b2eaaa75-df25-4419-8b64-48f87be986ba.png",
+  "/gallery/arts/812906ad-bccc-4206-a48c-a0f558e41f64.png",
+  "/gallery/arts/2d412165-ecdf-4976-a142-5b00f95251cb.png",
+  "/gallery/arts/3759d3cd-9eea-4303-b2de-511c1bc94827.png",
+  "/gallery/arts/7571356b-dbb4-425c-a319-c5cb058d48fa.png"
 ];
 
 const photoImages = [
-  "/lovable-uploads/3079284f-103d-418b-af8d-437a44fad638.png",
-  "/lovable-uploads/4be0f5d6-6404-4327-9f4a-641192127249.png",
-  "/lovable-uploads/d65d8443-3477-49e7-bac5-caeec820d714.png",
-  "/lovable-uploads/e92a2158-0c0c-4f5f-b07b-a0c70b89c20d.png",
-  "/lovable-uploads/09dae5f5-7634-4749-9a0b-fbe85fd6ca93.png",
-  "/lovable-uploads/3e6d02db-f636-4afd-bf53-b0a6513d0fd9.png",
-  "/lovable-uploads/efac75c3-9222-4a8e-ae93-3f55dc4607b2.png",
-  "/lovable-uploads/0831a829-d4c0-4707-82ec-57ee65bb6a13.png",
-  "/lovable-uploads/74d838f1-4aca-4121-baa1-4771a26377dd.png"
+  "/gallery/pictures/3079284f-103d-418b-af8d-437a44fad638.png",
+  "/gallery/pictures/4be0f5d6-6404-4327-9f4a-641192127249.png",
+  "/gallery/pictures/d65d8443-3477-49e7-bac5-caeec820d714.png",
+  "/gallery/pictures/e92a2158-0c0c-4f5f-b07b-a0c70b89c20d.png",
+  "/gallery/pictures/09dae5f5-7634-4749-9a0b-fbe85fd6ca93.png",
+  "/gallery/pictures/3e6d02db-f636-4afd-bf53-b0a6513d0fd9.png",
+  "/gallery/pictures/efac75c3-9222-4a8e-ae93-3f55dc4607b2.png",
+  "/gallery/pictures/e92a2158-0c0c-4f5f-b07b-a0c70b89c24d.png",
+  "/gallery/pictures/e92a2158-0c0c-4f5f-b07b-a0c70b89c25d.png",
+  "/gallery/pictures/e92a2158-0c0c-4f5f-b07b-a0c70b89c26d.png",
+  "/gallery/pictures/0831a829-d4c0-4707-82ec-57ee65bb6a13.png",
+  "/gallery/pictures/74d838f1-4aca-4121-baa1-4771a26377dd.png"
 ];
 
 const formatDate = (date: string) => {
@@ -333,7 +336,7 @@ const Index = () => {
                 "before:blur-md"
               )}>
                 <img 
-                  src="/lovable-uploads/98832de4-7399-438e-8f82-9625067892d8.png" 
+                  src="/gallery/arts/98832de4-7399-438e-8f82-9625067892d8.png"
                   alt="Profile Avatar" 
                   className="w-full h-full object-cover rounded-full border-2 border-purple relative z-10 animate-[float_6s_ease-in-out_infinite]" 
                 />
@@ -363,7 +366,7 @@ const Index = () => {
                   
                   <div className="opacity-0 animate-[fadeInUp_0.8s_ease-out_1.1s_forwards]">
                     <img 
-                      src="/lovable-uploads/43cef4b8-3c64-4f7b-a42f-5784dc8c3338.png" 
+                      src="/gallery/arts/43cef4b8-3c64-4f7b-a42f-5784dc8c3338.png"
                       alt="Danny Lisitsky Fursona" 
                       className="mx-auto max-w-full h-auto rounded-lg border-2 border-purple my-4 transform hover:scale-105 transition-all duration-500 shadow-lg shadow-purple/20 w-3/4 hover:shadow-xl hover:shadow-purple/30" 
                     />
